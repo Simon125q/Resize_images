eda2c5cad3d330d4ad27ed93f9718265533988b2
Simon125q <sz.omiecinski@gmail.com>
2023-07-23 15:12:03 +0200

    working version

--- resize.py
+++ resize.py
@@ -3,16 +3,22 @@ import os
 
 RESIZE_FACTOR = 2
 
-path_in = 'C:/Users/szomi/Dropbox/Komputer/Desktop/NinjaAdventure'
-path_out = 'C:/Users/szomi/Dropbox/Komputer/Desktop/graphics/32x32'
+path_in = 'c:/Users/szomi/Dropbox/Komputer/Desktop/NinjaAdventure'
+path_out = 'c:/Users/szomi/Dropbox/Komputer/Desktop/graphics/32x32'
 
-for path, dir_names, file_names in os.walk(path_out):
+for path, dir_names, file_names in os.walk(path_in):
+    if not os.path.isdir(path.replace(path_in, path_out)):
+                os.mkdir(path.replace(path_in, path_out))
     for file in file_names:
-        if file[-4:] == '.png': #and file[0] == "C":
-            print(path.replace(path_in, path_out))
-            # image = Image.open(path + '/' + file)
-            # width, height = image.size
-            # new_size = (width * RESIZE_FACTOR, height * RESIZE_FACTOR)
-            # resized_image = image.resize(new_size, resample = 0)
-
-            #resized_image.save(path_out + path.replace)
+        if file[-4:] == '.png':
+            p = path
+            image = Image.open(path + '/' + file)
+            p = p.replace(path_in, path_out)
+            p = p.replace('\\', '/')
+            p = p.strip()
+            width, height = image.size
+            new_size = (width * RESIZE_FACTOR, height * RESIZE_FACTOR)
+            resized_image = image.resize(new_size, resample = 0)
+            
+            p = p + '/' + file
+            resized_image.save(p)
